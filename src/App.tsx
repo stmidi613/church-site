b1770291f9112f3acdf77d9310c7cbae95e3e346
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-11-28 14:47:53 +0900

    I added the pictures for the background images and touched up the header portion by adding colors and responsive layout.

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,39 @@ import "./App.css";
 function App() {
   return (
     <Fragment>
-
-    <header tabIndex={-1}>
-      <section className="top-header">
-        <div className="title-group">
-          <img src="" alt="十字架" />
-          <p className="title">新約聖書クリスチャン教会日本</p>
-        </div>
-        <nav className="navbar">
-          <ul className="nav-links">
-            <li>
-              <a href="#">ホームパージ</a>
-            </li>
-            <li>
-              <a href="#">教会の内容</a>
-            </li>
-            <li>
-              <a href="#">連絡先</a>
-            </li>
-            <li>
-              <a href="#">勉強ガイド</a>
-            </li>
-            <li>
-              <a href="#">エベントの参加</a>
-            </li>
-          </ul>
-        </nav>
-        <div className="log-in-group">
-          <img src="" alt="プロファイル写真" />
-          <p>ログイン</p>
-        </div>
-      </section>
-    </header>
-
-        
+      <header
+        tabIndex={-1}
+        className="bg-[url(./images/cross-bg-sm.jpg)] bg-contain md:bg-[url(./images/cross-bg-lg.jpg)] md:bg-cover"
+      >
+        <section className="top-header">
+          <div className="md:flex justify-center m-5 hidden">
+            <nav className="navbar">
+              <ul className="nav-links">
+                <li>
+                  <a href="/#">ホームパージ</a>
+                </li>
+                <li>
+                  <a href="/#">教会の内容</a>
+                </li>
+                <li>
+                  <a href="/#">勉強ガイド</a>
+                </li>
+                <li>
+                  <a href="/#">エベントの参加</a>
+                </li>
+                <li>
+                  <a href="/#">連絡先</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="title-group m-5">
+            <p className="title text-2xl md:text-5xl">
+              新約聖書クリスチャン<span className="break-words">教会日本</span>
+            </p>
+          </div>
+        </section>
+      </header>
     </Fragment>
   );
 }
