0734a4a4429c104d20b777a4781a16fc2b362185
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-12-07 19:03:58 +0900

    All cards with new picture s have been added with map function. Will move to next section.

--- src/Home/Features/Cards.tsx
+++ src/Home/Features/Cards.tsx
@@ -1,24 +1,54 @@
 import React from "react";
 
+import SingleCard from "./SingleCard";
+
 import cross from "../../images/cross-bg-sm.jpg";
+import Bible from "../../images/Bible.jpg";
+import contactUs from "../../images/contact-us.jpg";
 
 const Cards = () => {
+  const CARDS: [
+    { cardId: number; heading: string; paragraph: string; picture: string; alt: string; },
+    { cardId: number; heading: string; paragraph: string; picture: string; alt: string; },
+    { cardId: number; heading: string; paragraph: string; picture: string; alt: string; }
+  ] = [
+    {
+      cardId: 1,
+      heading: "教会の情報",
+      paragraph:
+        "新約聖書クリスチャン教会日本は大阪市にあります。現在、聖書の学び会だけ行いますが、近い未来に礼拝も行うつもりです。誰でも参加してもよろしい、みんなを歓迎したいと思います。",
+      picture: `${cross}`,
+      alt: "A cross with the sun shining behind it."
+    },
+    {
+      cardId: 2,
+      heading: "聖書の学び会",
+      paragraph:
+        "Please come and study the Bible with us.  Learn together, grow together, live together.  For those who are curious and want to know more about Christianity we have easy to understand study guides to help you understand the Christian faith and what is in the Bible.",
+      picture: `${Bible}`,
+      alt: "A person holding a Bible with a path in the background."
+    },
+    {
+      cardId: 3,
+      heading: "連絡先",
+      paragraph:
+        "Please feel free to contact us.  We will try to get back to you as soon as possible.",
+      picture: `${contactUs}`,
+      alt: "Cell with apps for contacting people."
+    },
+  ];
   return (
     <>
-      <div className="grid place-content-center">
-        <div className="grid place-content-center w-[300px]">
-          <img width="300px" height="160px" src={cross} alt="A cross" />
-          <div className="bg-[#f7f6f2] text-center pb-8">
-            <h4 className="text-xl p-4">教会の情報</h4>
-            <p className="text-sm px-4 pb-8">
-              新約聖書クリスチャン教会日本は大阪市にあります。
-              現在、聖書の学び会だけ行いますが、
-              近い未来に礼拝も行うつもりです。誰でも参加してもよろしい、
-              みんなを歓迎したいと思います。
-            </p>
-            <a href="/#">もっと読む &gt;&gt;</a>
-          </div>
-        </div>
+      <div className="grid place-content-center md:flex md:justify-around gap-4">
+        {CARDS.map((card) => (
+          <SingleCard
+            key={card.cardId}
+            heading={card.heading}
+            paragraph={card.paragraph}
+            picture={card.picture}
+            alt={card.alt}
+          />
+        ))}
       </div>
     </>
   );
---
+++ src/Home/Features/SingleCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface Props {
+  heading: string;
+  paragraph: string;
+  picture: string;
+  alt: string;
+}
+
+const SingleCard: React.FC<Props> = ({ heading, paragraph, picture, alt }) => {
+  return (
+    <div className="pb-6 grid place-content-center w-[300px]">
+      <img
+        className="h-[200px]"
+        width="640px"
+        height="479px"
+        src={picture}
+        alt={alt}
+      />
+      <div className="bg-[#f7f6f2] pb-8j h-[300px] relative">
+        <h4 className="text-center text-xl p-4">{heading}</h4>
+        <p className="text-center text-sm px-4 pb-8">{paragraph}</p>
+        <a className="w-full text-center absolute bottom-0 py-6" href="/#">
+          もっと読む &gt;&gt;
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
