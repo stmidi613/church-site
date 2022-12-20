fb85da726ed94d62e05032323473f80be40005f1
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-12-20 17:43:05 +0900

    I added a StudyGuides.tsx Component and put in the Navbar and footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Fragment } from "react";
 
 import LandingPage from "./Home/LandingPage";
+import StudyGuides from "./StudyGuides/StudyGuides";
 
 import "./App.css";
 
 function App() {
   return (
     <Fragment>
-      <LandingPage />
+      <StudyGuides />
     </Fragment>
   );
 }
---
+++ src/StudyGuides/StudyGuides.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+
+import "../Home/Hero/Hero.css"
+
+const StudyGuides = () => {
+  return (
+    <>
+    <section>
+
+      <Navbar />
+    </section>
+      <div className="w-full h-96 bg-gold"></div>
+      <Footer />
+    </>
+  );
+};
+
+export default StudyGuides;
