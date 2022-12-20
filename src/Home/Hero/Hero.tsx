dbf92b9c38b269d6a4bece03b5c370006c9c4887
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-12-20 17:14:38 +0900

    Added a div before nav element so every page does not need a buffer for the fixed navbar.  Also created a Navbar folder and component.

--- src/Home/Hero/Hero.tsx
+++ src/Home/Hero/Hero.tsx
@@ -1,66 +1,25 @@
-import React from 'react'
+import React from "react";
 
-import "./Hero.css"
+import Navbar from "../../Navbar/Navbar";
+
+import "./Hero.css";
 
 const Hero = () => {
   return (
     <header
-        tabIndex={-1}
-        className="min-h-[300px] h-[50vh] md:h-[80vh] bg-[url(./images/cross-bg-sm.jpg)] bg-cover md:bg-[url(./images/cross-bg-lg.jpg)] md:bg-cover"
-      >
-        <section className="top-header">
-          <div className="flex justify-center">
-            {/* regular navbar */}
-            <nav className="navbar bg-black fixed z-10 p-1">
-              <ul className="hidden nav-links md:flex">
-                <li>
-                  <a href="/#">ホームパージ</a>
-                </li>
-                <li>
-                  <a href="/#">教会の情報</a>
-                </li>
-                <li>
-                  <a href="/#">勉強ガイド</a>
-                </li>
-                <li>
-                  <a href="/#">エベントの参加</a>
-                </li>
-                <li>
-                  <a href="/#">連絡先</a>
-                </li>
-              </ul>
-              {/* collapsed navbar */}
-              <div id="hamburger-container" className="md:hidden">
-                <label htmlFor="hamburger">&#9776;</label>
-                <input type="checkbox" id="hamburger" />
-                <ul id="hamburger-background">
-                  <li className="hamburger-item">
-                    <a href="/#">ホームパージ</a>
-                  </li>
-                  <li className="hamburger-item">
-                    <a href="/#">教会の情報</a>
-                  </li>
-                  <li className="hamburger-item">
-                    <a href="/#">勉強ガイド</a>
-                  </li>
-                  <li className="hamburger-item">
-                    <a href="/#">エベントの参加</a>
-                  </li>
-                  <li className="hamburger-item">
-                    <a href="/#">連絡先</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </div>
-          <div className="title-group p-8">
-            <h1 className="title text-2xl md:text-5xl pt-6">
-              新約聖書クリスチャン<span className="break-all">教会日本</span>
-            </h1>
-          </div>
-        </section>
-      </header>
-  )
-}
+      tabIndex={-1}
+      className="min-h-[300px] h-[50vh] md:h-[80vh] bg-[url(./images/cross-bg-sm.jpg)] bg-cover md:bg-[url(./images/cross-bg-lg.jpg)] md:bg-cover"
+    >
+      <section className="top-header">
+        <Navbar />
+        <div className="title-group p-8">
+          <h1 className="title text-2xl md:text-5xl">
+            新約聖書クリスチャン<span className="break-all">教会日本</span>
+          </h1>
+        </div>
+      </section>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="w-full bg-black static h-[48px]"></div>
+        {/* regular navbar */}
+        <nav className="navbar bg-black fixed z-10 p-1">
+          <ul className="hidden nav-links md:flex">
+            <li>
+              <a href="/#">ホームパージ</a>
+            </li>
+            <li>
+              <a href="/#">教会の情報</a>
+            </li>
+            <li>
+              <a href="/#">勉強ガイド</a>
+            </li>
+            <li>
+              <a href="/#">エベントの参加</a>
+            </li>
+            <li>
+              <a href="/#">連絡先</a>
+            </li>
+          </ul>
+          {/* collapsed navbar */}
+          <div id="hamburger-container" className="md:hidden">
+            <label htmlFor="hamburger">&#9776;</label>
+            <input type="checkbox" id="hamburger" />
+            <ul id="hamburger-background">
+              <li className="hamburger-item">
+                <a href="/#">ホームパージ</a>
+              </li>
+              <li className="hamburger-item">
+                <a href="/#">教会の情報</a>
+              </li>
+              <li className="hamburger-item">
+                <a href="/#">勉強ガイド</a>
+              </li>
+              <li className="hamburger-item">
+                <a href="/#">エベントの参加</a>
+              </li>
+              <li className="hamburger-item">
+                <a href="/#">連絡先</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
