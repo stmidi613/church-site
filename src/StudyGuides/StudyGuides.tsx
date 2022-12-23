971ae7b69467488b4189af2b988f04f39fe29937
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-12-23 20:36:24 +0900

    Completed the section by replacing the picture and also added text and made it white.

--- src/StudyGuides/StudyGuides.tsx
+++ src/StudyGuides/StudyGuides.tsx
@@ -9,8 +9,14 @@ const StudyGuides = () => {
   return (
     <>
       <Navbar />
-      <section>
-        <div className="w-full h-80">
+      <section className="w-full h-80 md:h-[60vh] bg-cover bg-[url(./images/studying.jpg)]">
+        <div className="grid place-content-center h-full w-full bg-gray/50">
+          <div className="text-center">
+            <h2 className="text-3xl text-white pb-4">勉強ガイド</h2>
+            <p className="text-white px-8">
+              ぜひ勉強ガイドを自由に使ってください。聖書やキリスト教に興味がある人のために作られた勉強ガイドには簡単な言葉があります。さらに、分かりやすくて説明があるので学ぶことは簡単で楽しいです。
+            </p>
+          </div>
         </div>
       </section>
       <Footer />
