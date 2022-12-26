4dc0938e0d4a3c6f6a6163a93b89d321895ff9b9
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-12-26 11:29:50 +0900

    changed the header component css so section height is taller for larger screens and small text is easier to read

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -9,12 +9,12 @@ interface Props {
 const Header: React.FC<Props> = ({backgroundImage, title, paragraph}) => {
   return (
     <section
-      className={`w-full h-80 md:h-[60vh] bg-cover z-0 ${backgroundImage}`}
+      className={`w-full h-80 md:h-[60vh] lg:h-[75vh] bg-cover ${backgroundImage}`}
     >
-      <div className="grid place-content-center h-full w-full bg-gray/40 z-10">
+      <div className="grid place-content-center h-full w-full bg-gray/40">
         <div className="text-center">
-          <h2 className="text-3xl text-white pb-4">{title}</h2>
-          <p className="text-white px-8">{paragraph}</p>
+          <h2 className="text-5xl text-white pb-4">{title}</h2>
+          <p className="text-white bg-gray/50 px-8">{paragraph}</p>
         </div>
       </div>
     </section>
