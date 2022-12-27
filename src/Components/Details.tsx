95fba1bca5e728779c958fdb36c10cdca19eee5f
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-12-27 12:49:31 +0900

    Main Content Section completed.

--- src/Components/Details.tsx
+++ src/Components/Details.tsx
@@ -3,13 +3,12 @@ import React from "react";
 interface Props {
   detailsTitle: string;
   detailsParagraph: string;
-  backgroundColor?: string;
 }
 
-const Details: React.FC<Props> = ({ detailsTitle, detailsParagraph, backgroundColor }) => {
+const Details: React.FC<Props> = ({ detailsTitle, detailsParagraph }) => {
   return (
     <>
-      <section aria-labelledby="details" className={`text-center text-gray ${backgroundColor}`}>
+      <section aria-labelledby="details" className="text-center text-gray">
         <h3 className="p-8 text-lg">{detailsTitle}</h3>
         <div className="w-full grid place-content-center">
           <div className="w-[25vw] text-sm border-gray border-t-2"></div>
