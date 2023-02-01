91576f863cc8dfc7bdcc45814ae27253a149a7c6
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2023-02-02 01:25:31 +0900

    added the events details section and main content section.

--- src/Pages/Events/Events.tsx
+++ src/Pages/Events/Events.tsx
@@ -2,12 +2,21 @@ import React from "react";
 
 import Navbar from "../../Components/Navbar/Navbar";
 import Header from "../../Components/Header";
+import Details from "../../Components/Details/Details";
+import MainContent from "../../Components/MainContent/MainContent";
+import AboutUsImage from "../../Components/MainContent/AboutUsImage";
 import Footer from "../../Components/Footer/Footer";
 
 const Events = () => {
-const backgroundImage: string = "bg-handheart";
-const title: string = "集会・エベント"
-const paragraph: string = "神様について学んだり、友達ができたり、賛美を歌ったり、することは楽しいです。神様の臨在のなかで一緒に生きたり、笑ったり、愛したり、しましょう。"
+  const backgroundImage: string = "bg-handheart";
+  const title: string = "集会・エベント";
+  const paragraph: string =
+    "神様について学んだり、友達ができたり、賛美を歌ったり、することは楽しいです。神様の臨在のなかで一緒に生きたり、笑ったり、愛したり、しましょう。";
+
+  const details: { detailsTitle: string; detailsParagraph: string } = {
+    detailsTitle: "神様のご家族に入りましょう",
+    detailsParagraph: "イエス様の言葉は「あなたの隣人をあなた自身のように愛せよ。」私たちは隣の人だけはなくて、家族のように歓迎したいと思います。初めて教会に行っても、キリスト教にちょっと興味があっても、参加するとよろしくお願い申し上げます。",
+  };
 
   return (
     <>
@@ -17,6 +26,11 @@ const paragraph: string = "神様について学んだり、友達ができた�
         title={title}
         paragraph={paragraph}
       />
+      <Details
+        detailsTitle={details.detailsTitle}
+        detailsParagraph={details.detailsParagraph}
+      />
+      <MainContent button={false} image={<AboutUsImage />} detailsTitle={"時間割"} detailsParagraph={""} quote={""} verse={""} />
       <Footer />
     </>
   );
