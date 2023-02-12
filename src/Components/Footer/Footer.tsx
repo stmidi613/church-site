91cacc061fc2223991344ef72a2175ebb167dc09
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2023-02-13 04:10:57 +0900

    added the navbar title and added all the icons to the footer for social media links

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,10 +3,18 @@ import React from "react";
 import twitter from "../../images/twitter.png";
 import linkedIn from "../../images/LinkedIn.png";
 import email from "../../images/Email.png";
+import meetup from "../../images/meetup.png";
+import line from "../../images/line.png";
 
 const Footer = () => {
-  const ICONS: string[] = [twitter, linkedIn, email];
-  const ALTS: string[] = ["twitter icon", "linked in icon", "email icon"];
+  const ICONS: string[] = [twitter, linkedIn, email, meetup, line];
+  const ALTS: string[] = [
+    "twitter icon",
+    "linked in icon",
+    "email icon",
+    "meetup icon",
+    "line icon",
+  ];
 
   return (
     <>
@@ -14,30 +22,32 @@ const Footer = () => {
         className="grid place-content-center bg-black text-darkgold text-left"
         aria-labelledby="footer"
       >
-        <div className="grid place-content-center md:flex md:justify-around">
-          <div className="p-8 md:w-1/2">
-            <h2 className="text-2xl pb-2">
-              新約聖書クリスチャン教会にようこそ！
-            </h2>
-            <p className="text-sm">
-              私たちはみんなを歓迎したいと思います。新約聖書クリスチャン教会のエベントを参加すると喜びます。ぜひ遊びに来たら、連絡したり、してもよろしいです。
-            </p>
+        <div className="grid lg:flex">
+          <div className="grid place-content-center md:flex md:justify-around">
+            <div className="p-8 md:w-1/2 lg:w-1/3">
+              <h2 className="text-2xl pb-2">
+                新約聖書クリスチャン教会にようこそ！
+              </h2>
+              <p className="text-sm">
+                私たちはみんなを歓迎したいと思います。新約聖書クリスチャン教会のエベントを参加すると喜びます。ぜひ遊びに来たら、連絡したり、してもよろしいです。
+              </p>
+            </div>
+            <div className="p-8 lg:w-1/3">
+              <h2 className="text-2xl pb-2">場所</h2>
+              <p className="text-sm pb-2">080-9180-9239</p>
+              <p className="text-sm">大阪府大阪市西成区萩之茶屋</p>
+            </div>
           </div>
-          <div className="p-8">
-            <h2 className="text-2xl pb-2">場所</h2>
-            <p className="text-sm pb-2">080-9180-9239</p>
-            <p className="text-sm">大阪府大阪市西成区萩之茶屋</p>
+          <div className="p-4 pb-8 lg:w-1/3 flex flex-wrap justify-around items-center gap-2">
+              {ICONS.map((icon, index) => (
+                <img
+                  key={index}
+                  className="icons transition hover:filter hover:brightness-125 hover:ease-in-out duration-500 w-12 h-12 p-2 hover:bg-gray rounded-md"
+                  src={icon}
+                  alt={ALTS[index]}
+                />
+              ))}
           </div>
-        </div>
-        <div className="pb-8 flex justify-around">
-          {ICONS.map((icon, index) => (
-            <img
-              key={index}
-              className="icons transition hover:filter hover:brightness-125 hover:ease-in-out duration-500 w-12 p-2 hover:bg-gray rounded-md"
-              src={icon}
-              alt={ALTS[index]}
-            />
-          ))}
         </div>
       </footer>
     </>
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -8,49 +8,64 @@ const Navbar = () => {
   return (
     <>
       <div className="flex justify-center">
-        <div className="w-full bg-black static h-[48px]"></div>
+        <div className="w-full bg-black static h-[52px] md:h-[116px]"></div>
         {/* regular navbar */}
-        <nav className="navbar bg-black fixed z-10 p-1">
-          <ul className="hidden nav-links md:flex">
-            <li>
-              <Link to="/">ホームパージ</Link>
-            </li>
-            <li>
-              <Link to="/AboutUs">教会の情報</Link>
-            </li>
-            <li>
-              <Link to="/StudyGuides">勉強ガイド</Link>
-            </li>
-            <li>
-              <Link to="/Events">集会・エベント</Link>
-            </li>
-            <li>
-              <Link to="/ContactUs">連絡先</Link>
-            </li>
-          </ul>
-          {/* collapsed navbar */}
-          <div id="hamburger-container" className="md:hidden">
-            <label htmlFor="hamburger">&#9776;</label>
-            <input type="checkbox" id="hamburger" />
-            <ul id="hamburger-background">
-              <li className="hamburger-item">
+        <div className="fixed w-full z-10">
+          <div className="hidden bg-black text-darkgold w-full text-center text-3xl p-4 md:block">
+            新約聖書クリスチャン教会
+          </div>
+          <nav className="navbar bg-black p-1">
+            <ul className="hidden nav-links md:flex">
+              <li>
                 <Link to="/">ホームパージ</Link>
               </li>
-              <li className="hamburger-item">
+              <li>
                 <Link to="/AboutUs">教会の情報</Link>
               </li>
-              <li className="hamburger-item">
+              <li>
                 <Link to="/StudyGuides">勉強ガイド</Link>
               </li>
-              <li className="hamburger-item">
-                <Link to="/Events">集会・エベント</Link>
+              <li>
+                <Link to="/Events">集会</Link>
               </li>
-              <li className="hamburger-item">
+              <li>
+                <Link to="/PrayerRequest">祈り求め</Link>
+              </li>
+              <li>
                 <Link to="/ContactUs">連絡先</Link>
               </li>
             </ul>
-          </div>
-        </nav>
+            {/* collapsed navbar */}
+            <div id="hamburger-container" className="md:hidden">
+              <label htmlFor="hamburger">
+                <span className="pt-3 text-xl text-darkgold">新約聖書クリ</span>
+                &#9776;
+                <span className="pt-3 text-xl text-darkgold">スチャン教会</span>
+              </label>
+              <input type="checkbox" id="hamburger" />
+              <ul id="hamburger-background">
+                <li className="hamburger-item">
+                  <Link to="/">ホームパージ</Link>
+                </li>
+                <li className="hamburger-item">
+                  <Link to="/AboutUs">教会の情報</Link>
+                </li>
+                <li className="hamburger-item">
+                  <Link to="/StudyGuides">勉強ガイド</Link>
+                </li>
+                <li className="hamburger-item">
+                  <Link to="/Events">集会・エベント</Link>
+                </li>
+                <li className="hamburger-item">
+                  <Link to="/PrayerRequest">祈り求め</Link>
+                </li>
+                <li className="hamburger-item">
+                  <Link to="/ContactUs">連絡先</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
       </div>
     </>
   );
