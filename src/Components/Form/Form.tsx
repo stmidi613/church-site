e388681dab8b927f50f3722f558be99bae5f3d23
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2023-02-13 06:51:35 +0900

    The form functionality was added and Emailjs installed.  Everything is working.

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,48 +1,62 @@
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 
+import emailjs from "@emailjs/browser";
 import "../Form/Form.css";
 
 interface Props {
   request: string;
 }
-const Form:React.FC<Props> = ({request}) => {
+const Form: React.FC<Props> = ({ request }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
   });
 
+  const form: string | any = useRef();
+
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    console.log(formData);
+    emailjs
+      .sendForm(
+        "service_i62c75x",
+        "contact_form",
+        form.current,
+        "KWksnCjkJ-zXZFkh_"
+      )
+      .then(
+        (result) => {
+          alert(result.text + "! Thank you for contacting us.  We have received your message.");
+        },
+        (error) => {
+          alert(error.text);
+        }
+      );
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form ref={form} onSubmit={handleSubmit}>
         <fieldset>
-          <label htmlFor="name">
-            氏名（ニックナームはよろしいです）
-          </label>
+          <input type="hidden" name="contact_number" />
+          <label htmlFor="name">氏名（ニックナームはよろしいです）</label>
           <input
             aria-required
             type="text"
             id="name"
-            name="name"
+            name="user-name"
             placeholder="本田・勇気"
             value={formData.name}
             onChange={(e) =>
               console.log(setFormData({ ...formData, name: e.target.value }))
             }
           />
-          <label htmlFor="email">
-            電子メール
-          </label>
+          <label htmlFor="email">電子メール</label>
           <input
             aria-required
             type="email"
             id="email"
-            name="email"
+            name="user_email"
             placeholder="yuukihonda123@gmail.com"
             value={formData.email}
             onChange={(e) =>
@@ -58,7 +72,7 @@ const Form:React.FC<Props> = ({request}) => {
             onChange={(e) =>
               console.log(setFormData({ ...formData, message: e.target.value }))
             }
-          ></textarea>
+          />
           <input
             className="transition hover:ease-in-out duration-500 text-xl md:text-xl lg:text-2xl mt-2 md:mt-4 text-darkgold bg-black rounded border-[1px] hover:bg-gray hover:text-gold"
             type="submit"
