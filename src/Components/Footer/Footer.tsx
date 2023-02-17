1d8cbae689462a96f9a213b705985d59406c33ab
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2023-02-18 02:00:42 +0900

    added the links and reconfigured the links

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -7,13 +7,32 @@ import meetup from "../../images/meetup.png";
 import line from "../../images/line.png";
 
 const Footer = () => {
-  const ICONS: string[] = [twitter, linkedIn, email, meetup, line];
-  const ALTS: string[] = [
-    "twitter icon",
-    "linked in icon",
-    "email icon",
-    "meetup icon",
-    "line icon",
+  const icons: { icon: string; alt: string; link: string }[] = [
+    {
+      icon: twitter,
+      alt: "twitter icon",
+      link: "https://twitter.com/NTCCofJapan",
+    },
+    {
+      icon: linkedIn,
+      alt: "linked in icon",
+      link: "https://www.linkedin.com/company/ntcc-of-japan",
+    },
+    {
+      icon: email,
+      alt: "email icon",
+      link: "mailto:ntcc_of_japan@outlook.jp?subject='Subject Text'",
+    },
+    {
+      icon: meetup,
+      alt: "meet up icon",
+      link: "https://www.meetup.com/ja-JP/%E6%96%B0%E7%B4%84%E8%81%96%E6%9B%B8%E3%82%AF%E3%83%AA%E3%82%B9%E3%83%81%E3%83%A3%E3%83%B3%E6%95%99%E4%BC%9A-ntcc-of-japan/",
+    },
+    {
+      icon: line,
+      alt: "line icon",
+      link: "https://page.line.me/153esgvt",
+    },
   ];
 
   return (
@@ -39,14 +58,16 @@ const Footer = () => {
             </div>
           </div>
           <div className="p-4 pb-8 lg:w-1/3 flex flex-wrap justify-around items-center gap-2">
-              {ICONS.map((icon, index) => (
+            {icons.map((icon, index) => (
+              <a href={icon.link} target="_blank" rel="noreferrer">
                 <img
                   key={index}
                   className="icons transition hover:filter hover:brightness-125 hover:ease-in-out duration-500 w-12 h-12 p-2 hover:bg-gray rounded-md"
-                  src={icon}
-                  alt={ALTS[index]}
+                  src={icon.icon}
+                  alt={icon.alt}
                 />
-              ))}
+              </a>
+            ))}
           </div>
         </div>
       </footer>
