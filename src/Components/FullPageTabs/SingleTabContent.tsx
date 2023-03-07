dccb01f0be096e9523a36e7a47233aeb1f531d62
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2023-03-07 17:39:16 +0900

    fixed and added pdfs to the pagetabs element

--- src/Components/FullPageTabs/SingleTabContent.tsx
+++ src/Components/FullPageTabs/SingleTabContent.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../../AppState/AppState";
 
 import SinglePDF from "./SinglePDF";
 
@@ -9,6 +10,10 @@ interface Props {
 }
 
 const SingleTabContent: React.FC<Props> = ({ tabNumber, number, title }) => {
+  const {Japanese} = useContext(AppContext)
+
+  const noFile: string = Japanese ? "申し訳ありませんが、只今ファイルがありません。" : "We are sorry but there are no files at this time."
+
   const outlines: {
     id: number;
     outlineName: string;
@@ -22,6 +27,7 @@ const SingleTabContent: React.FC<Props> = ({ tabNumber, number, title }) => {
       link: require("../../Pages/BibleSupport/PDF/heavyburdens2.pdf"),
     },
   ];
+  
   const bibleBasics: {
     id: number;
     name: string;
@@ -30,11 +36,36 @@ const SingleTabContent: React.FC<Props> = ({ tabNumber, number, title }) => {
   }[] = [
     {
       id: 1,
-      name: "重い重荷",
-      date: "2023/03/02",
-      link: require("../../Pages/BibleSupport/PDF/heavyburdens2.pdf"),
+      name: "聖書について事実",
+      date: "2022/12/08",
+      link: require("../../Pages/BibleSupport/PDF/TheBible.pdf"),
+    },
+    {
+      id: 2,
+      name: "聖書の本（旧約聖書）",
+      date: "2022/12/15",
+      link: require("../../Pages/BibleSupport/PDF/BibleBooks.pdf"),
+    },
+    {
+      id: 3,
+      name: "聖書の本（福音書）",
+      date: "2023/01/05",
+      link: require("../../Pages/BibleSupport/PDF/TheGospels.pdf"),
+    },
+    {
+      id: 4,
+      name: "神様の羊",
+      date: "2023/01/05",
+      link: require("../../Pages/BibleSupport/PDF/LambofGod.pdf"),
+    },
+    {
+      id: 5,
+      name: "神様の羊2",
+      date: "2023/01/05",
+      link: require("../../Pages/BibleSupport/PDF/LambofGod2.pdf"),
     },
   ];
+
   return (
     <>
       {tabNumber === number ? (
@@ -60,6 +91,8 @@ const SingleTabContent: React.FC<Props> = ({ tabNumber, number, title }) => {
               link={basic.link}
             /> : ""
           ))}
+          {number === 2 ? noFile : ""}
+          {number === 3 ? noFile : ""}
         </div>
       ) : (
         ""
