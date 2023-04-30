ef0dea3939a98a39cec4c0400c2099dd91be867d
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2023-05-01 01:40:14 +0900

    rebased the main branch and fixed the months on the dates

--- src/Pages/Home/Events/NextEvents.tsx
+++ src/Pages/Home/Events/NextEvents.tsx
@@ -19,28 +19,28 @@ const NextEvents = () => {
   Japanese ? [
     {
       eventId: 1,
-      name: "聖書の学び会",
-      date: "4月20日",
-      fullDate: "2023年04月20日（木曜日）",
+      name: "カフェでの付き合い",
+      date: "5月4日",
+      fullDate: "2023年05月04日（木曜日）",
     },
     {
       eventId: 2,
       name: "聖書の学び会",
-      date: "4月27日",
-      fullDate: "2023年04月27日（木曜日）",
+      date: "5月11日",
+      fullDate: "2023年05月11日（木曜日）",
     },
   ] : [
     {
       eventId: 1,
-      name: "Bible Study",
-      date: "4/20",
-      fullDate: "April 20, 2023 (Thursday)",
+      name: "Fellowship at a Cafe",
+      date: "4/04",
+      fullDate: "May 04, 2023 (Thursday)",
     },
     {
       eventId: 2,
       name: "Bible Study",
-      date: "4/27",
-      fullDate: "April 27, 2023 (Thursday)",
+      date: "4/11",
+      fullDate: "May 11, 2023 (Thursday)",
     },
   ];
 
